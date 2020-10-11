c927fee842aedc5b2741e7fa16f45d9e3dd1e3f4
Eduardo Salazar <eduarsalazar96@gmail.com>
2020-10-11 17:55:30 -0600

    Meta-layers for the first proyect for the curse Taller de Embedidos From Tecnológico de Costa Rica

---
+++ Proyecto_1/meta-bordes/recipes-edge/edge-1.0/Edge.py
@@ -0,0 +1,12 @@
+#import the required libraries
+import cv2
+import numpy as np
+
+img = cv2.imread('/home/root/myimages/face.jpg')
+edges = cv2.Canny(img,100,200)
+img = cv2.resize(img,(555,555),interpolation=4)
+edges = cv2.resize(edges,(555,555),interpolation=4)
+cv2.imshow('Original Image',img)
+cv2.imshow('Canny Edges After Contouring', edges) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows() 
